7b0ebbf05175f15aa1c2bcb928852d4d5300f244
Ria Kusmara <89031250+riakusmaraa@users.noreply.github.com>
2022-04-14 09:40:48 +0700

    Update HitungHargaTanah.java

--- HitungHargaTanah.java
+++ HitungHargaTanah.java
@@ -1,14 +1,16 @@
-public class Tugas3a {
+public class HitungHargaTanah {
     public static void main(String[] args) {
+    //deklarasi nilai
     int p = 12;
     int l = 8;
     int L, T, htanah = 500000;
     
+    //proses
     L = p*l;
     T = L * htanah;
     
-       
+    //output  
     System.out.println("Total harga tanah untuk luas: "+ L + " adalah "+T);
        
-}
+    }
 }
