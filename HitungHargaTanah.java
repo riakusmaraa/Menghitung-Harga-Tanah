d059c2a0e7d3229424159fa1e1a36d49f7c9f751
Ria Kusmara <89031250+riakusmaraa@users.noreply.github.com>
2022-04-14 09:35:33 +0700

    Add files via upload

---
+++ HitungHargaTanah.java
@@ -0,0 +1,14 @@
+public class Tugas3a {
+    public static void main(String[] args) {
+    int p = 12;
+    int l = 8;
+    int L, T, htanah = 500000;
+    
+    L = p*l;
+    T = L * htanah;
+    
+       
+    System.out.println("Total harga tanah untuk luas: "+ L + " adalah "+T);
+       
+}
+}
